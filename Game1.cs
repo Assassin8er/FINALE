3934853a23db3344cb3b952f3610fed5e24ea099
Cash Sullivan <Cashman7820@gmail.com>
2023-06-12 11:01:33 -0400

    Loaded more content and added background for main game.

--- Game1.cs
+++ Game1.cs
@@ -9,8 +9,8 @@ namespace FinalProject
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D asteroidTexture, flame1Texture, flame2Texture, shipTexture, backroundTexture;
-        Rectangle asteroidRect, shipRect, bulletRect, rocketRect, flame1Rect, flame2Rect;
+        Texture2D asteroidTexture, flame1Texture, flame2Texture, shipTexture, backroundTexture, shotTexture, spaceTexture;
+        Rectangle asteroidRect, shipRect, shotRect, rocketRect, flame1Rect, flame2Rect;
         Vector2 bulletspeed, asteroidSpeed, shipSpeed, rocketSpeed, flame1Speed, flame2Speed;
         SoundEffect Kaboom, Bam, Pew, Launch;
         SoundEffectInstance kaboomInstance, pewInstance, bamInstance, launchInstance;
@@ -33,6 +33,13 @@ namespace FinalProject
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            shipTexture = Content.Load<Texture2D>("Ship");
+            flame1Texture = Content.Load<Texture2D>("Flame1");
+            flame2Texture = Content.Load<Texture2D>("Flame2");
+            shotTexture = Content.Load<Texture2D>("shot");
+            spaceTexture = Content.Load<Texture2D>("Space0");
+
             Kaboom = Content.Load<SoundEffect>("Kaboom");
             Pew = Content.Load<SoundEffect>("Pew");
             Bam = Content.Load<SoundEffect>("Bam");
@@ -55,8 +62,11 @@ namespace FinalProject
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(spaceTexture, new Rectangle(0, 0, 1200, 800), Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
